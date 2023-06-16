ac74f39bbdcb1f3113b90bd79ce8cb2ba31b3cfb
Daniel Leal <daniel.leal@bhub.ai>
2023-06-16 20:52:51 -0300

    Fix prod configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,6 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :belle_donuts, BelleDonutsWeb.Endpoint,
-  url: [scheme: "https", host: System.get_env("PRODUCTION_URL"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
