e51fd27b838c366bcbe2bf30438e305f9af81adf
Daniel Leal <danielleal94@gmail.com>
2023-06-20 21:45:51 -0300

    Added cors configuration

--- lib/belle_donuts_web/endpoint.ex
+++ lib/belle_donuts_web/endpoint.ex
@@ -47,5 +47,6 @@ defmodule BelleDonutsWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: ["http://localhost:3000"]
   plug BelleDonutsWeb.Router
 end
